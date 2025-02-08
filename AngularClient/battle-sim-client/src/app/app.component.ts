91dfd805ba328b240c1eae27f1972a5ad9420fd9
d0rf47 <m.jay.falconi@gmail.com>
2025-02-07 21:54:53 -0500

    ng files

---
+++ AngularClient/battle-sim-client/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ButtonModule, FooterComponent, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'battle-sim-client';
+}
---
+++ AngularClient/battle-sim-client/src/app/header/header.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+import { CommonModule } from '@angular/common';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-header',
+  imports: [MenubarModule, CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  items: MenuItem[] = [];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.items = [
+      { label: 'Home', icon: 'pi pi-home', routerLink: ['/'] },
+      { label: 'Add Fighter', icon: 'pi pi-box', routerLink: ['/addfighter'] },
+      {
+        label: 'Account',
+        icon: 'pi pi-user',
+        items: [
+          { label: 'Profile', icon: 'pi pi-id-card', routerLink: ['/profile'] },
+          { label: 'Settings', icon: 'pi pi-cog', routerLink: ['/settings'] }
+        ]
+      }
+    ];
+  }
+}
